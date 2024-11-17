f18f105b4354927830a275019516ae65bdc3b1f9
marcos3777 <marcos_vinicius_11@hotmail.com>
2024-11-17 20:35:59 -0300

    refatuado menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -8,10 +8,10 @@ const Menu = () => {
     <div className="absolute top-full left-0 bg-white text-blue-800 border border-gray-300 rounded shadow-lg mt-2">
       <ul className="flex flex-col">
         <li className="px-4 py-2 hover:bg-blue-100 cursor-pointer">
-          <Link href="####">Login</Link>
+          <Link href="/login">Login</Link>
         </li>
         <li className="px-4 py-2 hover:bg-blue-100 cursor-pointer">
-          <Link href="####">Faça Parte</Link>
+          <Link href="/cadastro">Faça Parte</Link>
         </li>
       </ul>
     </div>
