bcc3474c87b2ec054f088bd0e3c837fc1097df83
fmelods <felipemelodesousa4@gmail.com>
2024-11-21 18:56:45 -0300

    Adicionada página de Dashboard com gráficos

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+// app/dashboard/page.tsx
+import React from "react";
+import Dashboard from "../../components/Dashboard/Dashboard"; // Ajuste o caminho, se necessário
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Dashboard />
+    </div>
+  );
+};
+
+export default DashboardPage;
